e380870add12db6fc925f957b420951b49d7b5e9
AndrewBSzabo <szabomon@outlook.com>
2020-04-03 23:07:39 -0400

    collisions and look at mouse added. interactions almost there

---
+++ Village Evacuation/Assets/DirectionalEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalEvent : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, Input.mousePosition - Input.mousePosition);
+    }
+}
