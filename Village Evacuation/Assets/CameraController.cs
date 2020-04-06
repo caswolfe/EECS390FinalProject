31e1dc2f27dcf50ab560c21d822afdb7175e6173
asfakianos <alexander@sfakianos.com>
2020-04-06 10:27:17 -0400

    Added Hotline Miami camera. There is currently a bug causing the character to disappear. Unsure of fix.

--- Village Evacuation/Assets/CameraController.cs
+++ Village Evacuation/Assets/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
 
     public GameObject player;
 
+    public float maxOffset;
+
     // Use this to keep track of the distance between the player and the camera
     private Vector3 offset;
 
@@ -19,7 +21,15 @@ public class CameraController : MonoBehaviour
     // LateUpdate is called after Update (each frame)
     void LateUpdate()
     {
-        // This will keep the camera fixed on the player, but we will need to change it when we want a more dynamic camera. 
+        // We still want to keep the camera following the player, so we use this to add offset
         transform.position = player.transform.position + offset;
+
+        // Dynamic Elements -- Find the difference between the mouse position and the camera position...
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
+        transform.position -= transform.position - mousePosition;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, player.transform.position.x - maxOffset, player.transform.position.x + maxOffset),
+                                         Mathf.Clamp(transform.position.y, player.transform.position.y - maxOffset, player.transform.position.y + maxOffset),
+                                         transform.position.y);
     }
 }
