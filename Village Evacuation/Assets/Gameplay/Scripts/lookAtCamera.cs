0212df44c8ce3e20d12f3cf3f155745fc1fd2bc0
asfakianos <alexander@sfakianos.com>
2020-04-07 15:26:06 -0400

    Fixed deadzone in last commit

--- Village Evacuation/Assets/Gameplay/Scripts/lookAtCamera.cs
+++ Village Evacuation/Assets/Gameplay/Scripts/lookAtCamera.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class lookAtCamera : MonoBehaviour
 {
 
-    void LateUpdate()
+    void Update()
     {
-        if (Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse Y") > 0)
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
         {
             var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
