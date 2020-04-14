a689e4dabbb4ace2b5b2aa6ef2a53401a465f539
Chad Wolfe <ChadAndrewWolfe@gmail.com>
2020-04-14 14:04:11 -0400

    added ui

---
+++ Village Evacuation/Assets/Gameplay/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public Text unsavedStudentsNumber;
+
+    public Text healthNumber;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
