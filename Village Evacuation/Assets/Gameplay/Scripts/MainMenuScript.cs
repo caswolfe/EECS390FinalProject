dee1f9abcc5b78362033636b6e784c9a2652a42f
Chad Wolfe <ChadAndrewWolfe@gmail.com>
2020-04-14 11:24:41 -0400

    got to go to managment

--- Village Evacuation/Assets/Gameplay/Scripts/MainMenuScript.cs
+++ Village Evacuation/Assets/Gameplay/Scripts/MainMenuScript.cs
@@ -8,4 +8,13 @@ public class MainMenuScript : MonoBehaviour
     public void onPlay(){
         SceneManager.LoadScene("House1");
     }
+
+    public void onCredits(){
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void onQuit(){
+        Application.Quit();
+    }
+
 }
