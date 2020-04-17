51e2bb0fdc9f0e59a539b23da9c0a759cd7f0265
Chad Wolfe <ChadAndrewWolfe@gmail.com>
2020-04-17 15:08:27 -0400

    working on GUI

--- Village Evacuation/Assets/Gameplay/Scripts/UIManager.cs
+++ Village Evacuation/Assets/Gameplay/Scripts/UIManager.cs
@@ -6,17 +6,63 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
 
+    public Canvas inGameUI;
+
+    public Canvas pauseUI;
+
+    public Canvas optionsUI;
+
+    public Image healthBarIndicator;
+
     public Text unsavedStudentsNumber;
 
-    public Text healthNumber;
+    public Text levelText;
+
+    private uiState currentState;
+
+    private int maxHealth = 100;
+
+    public enum uiState {
+        INGAME,
+        PAUSE,
+        OPTIONS
+    };
 
     void Start()
     {
-        
+        this.setUIState(uiState.INGAME);
     }
 
     void Update()
     {
         
     }
+
+    public void setUIState(uiState newState){
+        switch(newState){
+            case uiState.INGAME:
+                inGameUI.enabled = true;
+                pauseUI.enabled = false;
+                optionsUI.enabled = false;
+                break;
+            case uiState.PAUSE:
+                inGameUI.enabled = false;
+                pauseUI.enabled = true;
+                optionsUI.enabled = false;
+                break;
+            case uiState.OPTIONS:
+                inGameUI.enabled = false;
+                pauseUI.enabled = false;
+                optionsUI.enabled = true;
+                break;
+            default:
+                this.setUIState(uiState.INGAME);
+                break;
+        }
+    }
+
+    public void updateHealthDisplay(int newHealth){
+        
+    }
+
 }
