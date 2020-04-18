6664631fefdefc9316c4c24e0c6dbd6c07f066df
Chad Wolfe <ChadAndrewWolfe@gmail.com>
2020-04-18 00:10:29 -0400

    finished GUI, now only need to connect with game elements

--- Village Evacuation/Assets/Gameplay/Scripts/UIManager.cs
+++ Village Evacuation/Assets/Gameplay/Scripts/UIManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    public string levelName = "FILL LVL NAME";
 
     public Canvas inGameUI;
 
@@ -17,6 +20,8 @@ public class UIManager : MonoBehaviour
     public Text unsavedStudentsNumber;
 
     public Text levelText;
+
+    public Slider volumeSlider;
 
     private uiState currentState;
 
@@ -31,11 +36,27 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         this.setUIState(uiState.INGAME);
+        this.levelText.text = levelName;
     }
 
     void Update()
     {
-        
+        if(Input.GetKeyDown("escape")){
+            switch(currentState){
+                case uiState.INGAME:
+                    this.setUIState(uiState.PAUSE);
+                    break;
+                case uiState.PAUSE:
+                    this.setUIState(uiState.INGAME);
+                    break;
+                case uiState.OPTIONS:
+                    this.setUIState(uiState.PAUSE);
+                    break;
+                default:
+                    Debug.LogWarning("currentState undefined in UIManager");
+                    break;
+            }
+        }
     }
 
     public void setUIState(uiState newState){
@@ -44,25 +65,69 @@ public class UIManager : MonoBehaviour
                 inGameUI.enabled = true;
                 pauseUI.enabled = false;
                 optionsUI.enabled = false;
+                Cursor.visible = false;
+                // TODO: enable player input
                 break;
             case uiState.PAUSE:
                 inGameUI.enabled = false;
                 pauseUI.enabled = true;
                 optionsUI.enabled = false;
+                Cursor.visible = true;
+                // TODO: disable player input
                 break;
             case uiState.OPTIONS:
                 inGameUI.enabled = false;
                 pauseUI.enabled = false;
                 optionsUI.enabled = true;
+                Cursor.visible = true;
+                // TODO: disable player input
                 break;
             default:
                 this.setUIState(uiState.INGAME);
                 break;
         }
+        this.currentState = newState;
     }
 
-    public void updateHealthDisplay(int newHealth){
-        
+    public void setHealthDisplay(int newHealth){
+        float newXScale = ((float) newHealth) / ((float) maxHealth);
+        healthBarIndicator.transform.localScale = new Vector3(newXScale, 1.0f, 1.0f);
+    }
+
+    public void setUnsavedStudentsCount(int count){
+        unsavedStudentsNumber.text = count.ToString();
+    }
+
+    public void setLevel(string level){
+        levelText.text = level;
+    }
+
+    /*
+     * Action Events
+     */
+
+    public void onResume(){
+        this.setUIState(uiState.INGAME);
+    }
+
+    public void onOptions(){
+        this.setUIState(uiState.OPTIONS);
+    }
+
+    public void onMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void onQuitToDesktop(){
+        Application.Quit();
+    }
+
+    public void onOptionsBack(){
+        this.setUIState(uiState.PAUSE);
+    }
+
+    public void onVolumeLevelChange(){
+        Debug.Log("volume level now " + volumeSlider.value);
     }
 
 }
