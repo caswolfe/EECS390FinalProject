7e63c4654e28429780ef3ff7801c40bfb427fa33
Chad Wolfe <ChadAndrewWolfe@gmail.com>
2020-04-21 15:01:26 -0400

    added main menu volume slider

--- Village Evacuation/Assets/Gameplay/Scripts/MainMenuScript.cs
+++ Village Evacuation/Assets/Gameplay/Scripts/MainMenuScript.cs
@@ -2,9 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+
+    [Header("Components")]
+    public Slider volumeSlider;
+    public AudioSource[] audioSources;
+
+    void start(){
+        this.volumeSlider.SetValueWithoutNotify(SceneController.Instance.volume);
+        this.volumeSlider.value = SceneController.Instance.volume;
+    }
+
     public void onPlay(){
         SceneManager.LoadScene("House1");
     }
@@ -15,6 +26,18 @@ public class MainMenuScript : MonoBehaviour
 
     public void onQuit(){
         Application.Quit();
+    }
+
+    public void onVolumeLevelChange(){
+        // Debug.Log("volume level now " + volumeSlider.value);
+        SceneController.Instance.volume = volumeSlider.value;
+        this.setVolume(volumeSlider.value);
+    }
+
+    public void setVolume(float level){
+        foreach (AudioSource audioSource in audioSources){
+            audioSource.volume = level;
+        }
     }
 
 }
